71631703944db6212c174507c21fb87a4c979cdb
spell0227 <120394616+spell0227@users.noreply.github.com>
2023-05-25 20:35:16 +0800

    Update script.js

--- classifyGame/script.js
+++ classifyGame/script.js
@@ -420,7 +420,7 @@ function checkPlacement2() {
 
   // Show the score above the canvas
   var scoreElement = document.getElementById("score");
-  scoreElement.innerText = "得分： " + score;     //score:
+  scoreElement.innerText = "score:  " + score;     //score:
   // If there are wrong images, slide them to the middle and allow the user to place them again
   if (wrongImages.length > 0) {
     slideWrongImages(wrongImages);
@@ -468,7 +468,7 @@ function checkPlacement() {
 
   // Show the score above the canvas
   var scoreElement = document.getElementById("score");
-  scoreElement.innerText = "得分： " + score;
+  scoreElement.innerText = "score: " + score;
 
   // If there are wrong images, slide them to the middle and allow the user to place them again
   if (wrongImages.length > 0) {
