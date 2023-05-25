bbc39653d551c3c589558b8f19744fca2a4a06a1
spell0227 <120394616+spell0227@users.noreply.github.com>
2023-05-25 21:44:58 +0800

    Update script.js

--- classifyGameproducer/script.js
+++ classifyGameproducer/script.js
@@ -354,7 +354,7 @@ function drawAnswerBox(){
     ctx.rect(0 + i*boxWidth , 0, boxWidth, boxHeight);
     ctx.stroke();
     
-    ctx.font = "24px Arial";
+    ctx.font = "12px Arial";
     ctx.fillText(kindomNames[i], 0 + i*boxWidth + 10 , 30);    
   }
 
@@ -362,7 +362,7 @@ function drawAnswerBox(){
     ctx.beginPath();
     ctx.rect(0 + i * boxWidth , canvas.height *2/3 , boxWidth, boxHeight);
     ctx.stroke();
-    ctx.font = "24px Arial";
+    ctx.font = "12px Arial";
     ctx.fillText(kindomNames[i+4], 0 + i*boxWidth + 10 , canvas.height *2/3 + 30);      
   }
 }
